380cb10a37720b6bd80f4d033b0184ed5d4f8e1d
Timur Ismagulov <timur.ismagulov@gmail.com>
2022-06-15 13:44:12 +0200

    test tf modueles

---
+++ terraform/module/main.tf
@@ -0,0 +1,17 @@
+
+resource "aws_iam_role" "aws_ec2_custom_role" {
+  name = "test123"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
